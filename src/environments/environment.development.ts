8c00c52e74a12dcfc3b85ad6352c7f80f6162419
Ibrahim Ahmed <ibrahimahmed90000@gmail.com>
2023-10-20 23:17:18 +0300

    Replace the local API with a hosted API

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,6 @@ export const environment = {
   Production: false,
   Client_ID:
     'AcvNMt421l-NBJ7Mj836zlAUVBstJMALPNOr8MpJKDbH-qCd31vVrflOg-sOoB1uB8YNkLmtccg3t_gw',
-  BaseApiUrl: 'http://localhost:5775',
+  BaseApiUrl: 'http://ibrahimahmed289-001-site1.ctempurl.com',
+  // BaseApiUrl: 'http://localhost:5775',
 };
